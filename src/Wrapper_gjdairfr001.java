44687b012c7c5ecea6082e525361275f4d992af3
dcwy1130 <thisis_java@163.com>
2014-07-16 09:25:22 +0800

    Update Wrapper_gjdairfr001.java
    删除https处理

--- src/Wrapper_gjdairfr001.java
+++ src/Wrapper_gjdairfr001.java
@@ -116,10 +116,6 @@ public class Wrapper_gjdairfr001 implements QunarCrawler {
 		String toAirportIATA = InfoCenter.getCityFromAirportCode(arg0.getArr(),"EN") + "+" +StringUtils.substringBefore(InfoCenter.getAirportNameFromCode(arg0.getArr(), "EN"), " ");
 		//London+Stansted
 		try {
-			Protocol myhttps = new Protocol("https", new MySSLProtocolSocketFactory(), 443);  
-	         //注册刚才创建的https 协议对象  
-	         Protocol.registerProtocol("https", myhttps);   
-	         
 			QFHttpClient httpClient = new QFHttpClient(arg0, false);
 			httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
 
@@ -250,87 +246,4 @@ public class Wrapper_gjdairfr001 implements QunarCrawler {
 			flight.setInfo(segs);		
 		return flight;
 	}
-
-	/**https加密处理*/
-	 public class MySSLProtocolSocketFactory implements ProtocolSocketFactory {  
-	      
-	      private SSLContext sslcontext = null;   
-	       
-	      private SSLContext createSSLContext() {   
-	          SSLContext sslcontext=null;   
-	          try {   
-	              sslcontext = SSLContext.getInstance("SSL");   
-	              sslcontext.init(null, new TrustManager[]{(TrustManager) new TrustAnyTrustManager()}, new java.security.SecureRandom());   
-	          } catch (NoSuchAlgorithmException e) {   
-	              e.printStackTrace();   
-	          } catch (KeyManagementException e) {   
-	              e.printStackTrace();   
-	          }   
-	          return sslcontext;   
-	      }   
-	       
-	      private SSLContext getSSLContext() {   
-	          if (this.sslcontext == null) {   
-	              this.sslcontext = createSSLContext();   
-	          }   
-	          return this.sslcontext;   
-	      }   
-	       
-	      public Socket createSocket(Socket socket, String host, int port, boolean autoClose)   
-	              throws IOException, UnknownHostException {   
-	          return getSSLContext().getSocketFactory().createSocket(   
-	                  socket,   
-	                  host,   
-	                  port,   
-	                  autoClose   
-	              );   
-	      }   
-	      
-	      public Socket createSocket(String host, int port) throws IOException,   
-	              UnknownHostException {   
-	          return getSSLContext().getSocketFactory().createSocket(   
-	                  host,   
-	                  port   
-	              );   
-	      }   
-	       
-	       
-	      public Socket createSocket(String host, int port, InetAddress clientHost, int clientPort) throws IOException, UnknownHostException {   
-	          return getSSLContext().getSocketFactory().createSocket(host, port, clientHost, clientPort);   
-	      }   
-	      
-	      public Socket createSocket(String host, int port, InetAddress localAddress,   
-	              int localPort, HttpConnectionParams params) throws IOException,   
-	              UnknownHostException, ConnectTimeoutException {   
-	          if (params == null) {   
-	              throw new IllegalArgumentException("Parameters may not be null");   
-	          }   
-	          int timeout = params.getConnectionTimeout();   
-	          SocketFactory socketfactory = getSSLContext().getSocketFactory();   
-	          if (timeout == 0) {   
-	              return socketfactory.createSocket(host, port, localAddress, localPort);   
-	          } else {   
-	              Socket socket = socketfactory.createSocket();   
-	              SocketAddress localaddr = new InetSocketAddress(localAddress, localPort);   
-	              SocketAddress remoteaddr = new InetSocketAddress(host, port);   
-	              socket.bind(localaddr);   
-	              socket.connect(remoteaddr, timeout);   
-	              return socket;   
-	          }   
-	      }   
-	       
-	      //自定义私有类   
-	      private class TrustAnyTrustManager implements X509TrustManager {   
-	          
-	          public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {   
-	          }   
-	      
-	          public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {   
-	          }   
-	      
-	          public X509Certificate[] getAcceptedIssuers() {   
-	              return new X509Certificate[]{};   
-	          }   
-	      }      
-	    }
 }
